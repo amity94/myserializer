417a5f8439ddac54909742c27ae48d6678b54f29
amity94 <45635119+amity94@users.noreply.github.com>
2019-02-25 15:24:40 +0200

    Add files via upload

---
+++ hw2402/hw2402/Car.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace hw2402
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        private int codan;
+        protected int numberOfSeats;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string fileName)
+        {
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Car));
+
+            Car c;
+            using (Stream file = new FileStream($@"C:\temp\{fileName}.xml", FileMode.Open))
+            {
+                c = myXmlSerializer.Deserialize(file) as Car;
+            }//deserialize - can consle.writeline
+            Console.WriteLine(c);
+        }
+
+        public Car(string brand, string model, int year, string color, int codan, int numberOfSeats)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Year = year;
+            this.Color = color;
+            this.codan = codan;
+            this.numberOfSeats = numberOfSeats;
+        }
+
+        public int GetCodan()
+        {
+            return codan;
+        }
+
+        public int GetNumberOfSeats()
+        {
+            return numberOfSeats;
+        }
+
+        protected void ChangeNumberOfSeats(int number)
+        {
+            numberOfSeats = numberOfSeats + number;
+        }
+
+        public static void SerializeACar(string fileName, Car anycar)
+        {
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Car));
+
+            using (Stream file = new FileStream($@"C:\temp\{fileName}.xml", FileMode.Create))
+            {
+                myXmlSerializer.Serialize(file, anycar);
+            }//serialize - creates the file
+        }
+
+        public static void SerializeCarArray(string fileName, Car[] cars)
+        {
+            XmlSerializer myXmlSerializerArray = new XmlSerializer(typeof(Car[]));
+
+            using (Stream file = new FileStream($@"C:\temp\{fileName}.xml", FileMode.Create))
+            {
+                myXmlSerializerArray.Serialize(file, cars);
+            }//serialize - creates the file
+        }
+
+        public static Car DeserializeACar(string fileName)
+        {
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Car));
+
+            Car c;
+            using (Stream file = new FileStream($@"C:\temp\{fileName}.xml", FileMode.Open))
+            {
+                c = myXmlSerializer.Deserialize(file) as Car;
+            }//deserialize - can consle.writeline
+            return c;
+        }
+
+        public static Car[] DeserializeCarArray(string fileName)
+        {
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Car[]));
+
+            Car[] cars;
+            using (Stream file = new FileStream($@"C:\temp\{fileName}.xml", FileMode.Open))
+            {
+                cars = myXmlSerializer.Deserialize(file) as Car[];
+            }//deserialize - can consle.writeline
+            return cars as Car[];
+        }
+
+        //public static bool CarCompare(string fileName)
+        //{
+
+        //}
+
+        public override string ToString()
+        {
+            return $"Brand: {Brand, 10} Model: {Model, 15} Year: {Year, 4} Color: {Color, 15} Codan: {codan, 5} Number Of Seats: {numberOfSeats, 2}";
+        }
+
+    }
+}
---
+++ hw2402/hw2402/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace hw2402
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Car));
+
+            Car c1 = new Car("Honda", "Civic HatchBack", 2012, "Mouse Grey", 4215, 5);
+            using (Stream file = new FileStream(@"C:\temp\xmlfile.xml", FileMode.Create))
+            {
+                myXmlSerializer.Serialize(file, c1);
+            }//serializer - creates the file
+            using (Stream file = new FileStream(@"C:\temp\xmlfile.xml", FileMode.Open))
+            {
+                c1 = myXmlSerializer.Deserialize(file) as Car;
+            }//deserialize - can console.write
+            Console.WriteLine(c1);
+            Console.WriteLine();
+
+
+            Car c2 = new Car("Daihatsu", "Sirion", 2007, "Green/Grey", 1511, 5);
+            using (Stream file = new FileStream(@"C:\temp\xmlfile2.xml", FileMode.Create))
+            {
+                myXmlSerializer.Serialize(file, c2);
+            }//serialize - creates the file
+            using (Stream file = new FileStream(@"C:\temp\xmlfile2.xml", FileMode.Open))
+            {
+                c2 = myXmlSerializer.Deserialize(file) as Car;
+            }//deserialize - can console.write
+            Console.WriteLine(c2);
+            Console.WriteLine();
+
+
+            XmlSerializer myXmlSerializerArray = new XmlSerializer(typeof(Car[]));
+
+            Car[] cars = { c1, c2 };
+            using (Stream file = new FileStream(@"C:\temp\xmlfilearray.xml", FileMode.Create))
+            {
+                myXmlSerializerArray.Serialize(file, cars);
+            }//serialize aray - creates the file
+            using (Stream file = new FileStream(@"C:\temp\xmlfilearray.xml", FileMode.Open))
+            {
+                cars = myXmlSerializerArray.Deserialize(file) as Car[];
+            }//deserialize array - can console.write
+            foreach (var car in cars)
+            Console.WriteLine(car);
+            Console.WriteLine();
+
+
+            new Car("xmlfile");
+            new Car("xmlfile2");
+
+
+            Car.SerializeACar("xmlfile3", c1);
+            Car.SerializeCarArray("xmlfilearray2", cars);
+            Console.WriteLine();
+
+
+            Console.WriteLine(Car.DeserializeACar("xmlfile"));
+            Console.WriteLine(Car.DeserializeACar("xmlfile2"));
+
+
+            Console.WriteLine("Deserialize car array");
+            foreach (var car in cars)
+            Console.WriteLine(Car.DeserializeCarArray("xmlfilearray"));
+            
+            
+
+
+        }
+    }
+}
